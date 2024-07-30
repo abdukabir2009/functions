75d29e986a80e9e04e3312e98417eaaea6fe1368
abdukabir2009 <Abdukabirkh@gmail.com>
2024-07-30 16:50:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+// for (let i = 0; i < 10 ; i++) {
+//   console.log('hello');
+
+// }
+
+
+
+// let i = 0
+// while(i < 5) {
+//    console.log(i);
+//    i++
+// }
+
+
+
+
+// let i = 0
+// do {
+//     console.log(i);
+//     i++
+// } while (i < 10)
+
+
+
+
+// function sayHello(name) {
+//     console.log('salom' + name);
+// }
+
+// sayHello('Uzbek')
+
+
+
+
+// function sum(a, b) {
+//     console.log(a + b);
+// }
+
+// sum('nigga',1 ) 
+// sum('nigga',2 ) 
+// sum('nigga',3 ) 
+// sum('nigga',4 ) 
+
+
+
+
+
+// const greet = function(a) {
+//     console.log('uzbek ' + a);
+// }
+
+// greet('Uzbek')
+
+
+
+
+// const hi = (a, b) => a + b
+// console.log(hi(5, 5));
+
+// const nigga = (a, b) => a * b
+// console.log(nigga(5, 10));
+
+// const nig = (a, b) => a - b
+// console.log(nig(20, 15));
+
+// const uzb = (a, b) => a / b
+// console.log(uzb(5, 10));
+
+
+
+
+
+// const me = function(a, b, c) {
+//     console.log('Hello my name is ' + a + ' My surname ' + b + ' My age ' + c );
+// }
+
+
+
+
+const me = function(name, surname, age) {
+    console.log(`Hello my name is ${name} ${surname}, ${age} years old`);
+}
+
+me('Uzbek', 'Uzbekovich', 33)
